aa2566dc1c2144479217c40e978b163ad14e6da6
SprintKeyz <63497433+SprintKeyz@users.noreply.github.com>
2024-02-24 17:02:59 -0500

    Move filter button to navbar + add link button

--- app/src/main/java/com/sunkensplashstudios/VRCRoboScout/WorldSkillsView.kt
+++ app/src/main/java/com/sunkensplashstudios/VRCRoboScout/WorldSkillsView.kt
@@ -1,10 +1,6 @@
 package com.sunkensplashstudios.VRCRoboScout
 
-import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,36 +10,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -52,8 +52,6 @@ import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.saket.cascade.CascadeDropdownMenu
@@ -65,11 +63,12 @@ fun WorldSkillsView(navController: NavController) {
 
     var viewTitle by rememberSaveable { mutableStateOf("World Skills") }
     val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
 
     // filters
     var isFilteredByFavorites by remember { mutableStateOf(false) }
-    var hasFilteredLetter by remember { mutableStateOf(' ') }
-    var hasFilteredRegion by remember { mutableStateOf(0) }
+    var filteredLetter by remember { mutableStateOf(' ') }
+    var filteredRegion by remember { mutableIntStateOf(0) }
 
     // get favorite teams
     val favoriteTeams = remember {
@@ -80,34 +79,36 @@ fun WorldSkillsView(navController: NavController) {
     // clear filters
     fun clearFilters() {
         isFilteredByFavorites = false
-        hasFilteredLetter = ' '
-        hasFilteredRegion = 0
+        filteredLetter = ' '
+        filteredRegion = 0
         viewTitle = "World Skills"
     }
 
     // filter by favorites
     fun filterByFavorites() {
         isFilteredByFavorites = true
-        hasFilteredRegion = 0
-        hasFilteredLetter = ' '
+        filteredRegion = 0
+        filteredLetter = ' '
         viewTitle = "Favorites Skills"
     }
 
     // filter by letter
     fun filterByLetter(letter: Char) {
-        hasFilteredLetter = letter
-        hasFilteredRegion = 0
+        filteredLetter = letter
+        filteredRegion = 0
         isFilteredByFavorites = false
         viewTitle = "$letter Skills"
     }
 
     // filter by region
     fun filterByRegion(region: Int, regionName: String) {
-        hasFilteredRegion = region
+        filteredRegion = region
         isFilteredByFavorites = false
-        hasFilteredLetter = ' '
+        filteredLetter = ' '
         viewTitle = "$regionName Skills"
     }
+
+    var filterDropdownExpanded by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -118,6 +119,37 @@ fun WorldSkillsView(navController: NavController) {
                 ),
                 title = {
                     Text(viewTitle, fontWeight = FontWeight.Bold)
+                },
+                actions = {
+                    // link to world skills
+                    IconButton(
+                        onClick = {
+                            uriHandler.openUri("https://www.robotevents.com/robot-competitions/vex-robotics-competition/standings/skills")
+                        },
+                        modifier = Modifier.padding(horizontal = 5.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Link,
+                            contentDescription = "Open World Skills in Browser",
+                            tint = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.size(26.dp)
+                        )
+                    }
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            filterDropdownExpanded = true
+                        },
+                        modifier = Modifier.padding(horizontal = 5.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FilterList,
+                            contentDescription = "Filter",
+                            tint = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.size(26.dp)
+                        )
+                    }
                 }
             )
         }
@@ -136,6 +168,98 @@ fun WorldSkillsView(navController: NavController) {
             }
         }
 
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxWidth(),
+        ) {
+            CascadeDropdownMenu(
+                expanded = filterDropdownExpanded,
+                onDismissRequest = { filterDropdownExpanded = false },
+
+                modifier = Modifier
+                    .heightIn(max = 265.dp),
+                offset = DpOffset(10.dp, (-10).dp),
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Favorites") },
+                    onClick = {
+                        filterDropdownExpanded = false
+                        filterByFavorites()
+                    }
+                )
+
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 0.5.dp,
+                    modifier = Modifier.padding(horizontal = 5.dp)
+                )
+
+                DropdownMenuItem(
+                    text = { Text("Region") },
+                    children = {
+                        API.regionsMap.toSortedMap().forEach { (name, id) ->
+                            HorizontalDivider(
+                                color = Color.Gray,
+                                thickness = 0.5.dp,
+                                modifier = Modifier.padding(horizontal = 5.dp)
+                            )
+
+                            DropdownMenuItem(
+                                text = { Text(name) },
+                                onClick = {
+                                    filterByRegion(id, name)
+                                    filterDropdownExpanded = false
+                                }
+                            )
+                        }
+                    }
+                )
+
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 0.5.dp,
+                    modifier = Modifier.padding(horizontal = 5.dp)
+                )
+
+                DropdownMenuItem(
+                    text = { Text("Letter") },
+                    children = {
+                        // make a list of all the letters
+                        for (letter in 'A'..'Z') {
+                            HorizontalDivider(
+                                color = Color.Gray,
+                                thickness = 0.5.dp,
+                                modifier = Modifier.padding(horizontal = 5.dp)
+                            )
+
+                            DropdownMenuItem(
+                                text = { Text(letter.toString()) },
+                                onClick = {
+                                    filterByLetter(letter)
+                                    filterDropdownExpanded = false
+                                }
+                            )
+                        }
+                    }
+                )
+
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 0.5.dp,
+                    modifier = Modifier.padding(horizontal = 5.dp)
+                )
+
+                DropdownMenuItem(
+                    text = { Text("Clear Filters") },
+                    onClick = {
+                        clearFilters()
+                        filterDropdownExpanded = false
+                    }
+                )
+            }
+        }
+
         Column(
             modifier = Modifier
                 .padding(padding)
@@ -146,132 +270,6 @@ fun WorldSkillsView(navController: NavController) {
             } else if (API.wsCache.isEmpty()) {
                 NoDataView()
             } else {
-                // start filter menu retracted
-                var filterDropdownExpanded by remember { mutableStateOf(false) }
-
-                Card(
-                    shape = RectangleShape,
-                    modifier = Modifier
-                        .padding(0.dp, 1.dp, 0.dp, 15.dp)
-                        .fillMaxWidth()
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .indication(
-                                indication = null,
-                                interactionSource = remember { androidx.compose.foundation.interaction.MutableInteractionSource() }
-                            )
-                            .clickable {
-                                // toggle the filter dropdown
-                                filterDropdownExpanded = !filterDropdownExpanded
-                            }
-
-                    ) {
-                        Box(modifier = Modifier.align(Alignment.Center)) {
-                            Text(
-                                text = "Filter",
-                                color = MaterialTheme.colorScheme.primary,
-                                fontSize = 18.sp,
-                                // not sure why the iOS app has more padding at the bottom, but I guess I'll replicate it
-                                modifier = Modifier
-                                    .padding(bottom = 18.dp, top = 10.dp)
-                            )
-
-                            CascadeDropdownMenu(
-                                expanded = filterDropdownExpanded,
-                                onDismissRequest = { filterDropdownExpanded = false },
-                                // padding top -10dp
-                                offset = DpOffset(x = (-72).dp, y = (-12).dp),
-
-                                modifier = Modifier
-                                    .heightIn(max = 265.dp)
-
-                                // set max height to 200dp
-
-                            ) {
-                                DropdownMenuItem(
-                                    text = { Text("Favorites") },
-                                    onClick = {
-                                        filterDropdownExpanded = false
-                                        filterByFavorites()
-                                    }
-                                )
-
-                                HorizontalDivider(
-                                    color = Color.Gray,
-                                    thickness = 0.5.dp,
-                                    modifier = Modifier.padding(horizontal = 5.dp)
-                                )
-
-                                DropdownMenuItem(
-                                    text = { Text("Region") },
-                                    children = {
-                                        API.regionsMap.toSortedMap().forEach { (name, id) ->
-                                            HorizontalDivider(
-                                                color = Color.Gray,
-                                                thickness = 0.5.dp,
-                                                modifier = Modifier.padding(horizontal = 5.dp)
-                                            )
-
-                                            DropdownMenuItem(
-                                                text = { Text(name) },
-                                                onClick = {
-                                                    filterByRegion(id, name)
-                                                    filterDropdownExpanded = false
-                                                }
-                                            )
-                                        }
-                                    }
-                                )
-
-                                HorizontalDivider(
-                                    color = Color.Gray,
-                                    thickness = 0.5.dp,
-                                    modifier = Modifier.padding(horizontal = 5.dp)
-                                )
-
-                                DropdownMenuItem(
-                                    text = { Text("Letter") },
-                                    children = {
-                                        // make a list of all the letters
-                                        for (letter in 'A'..'Z') {
-                                            HorizontalDivider(
-                                                color = Color.Gray,
-                                                thickness = 0.5.dp,
-                                                modifier = Modifier.padding(horizontal = 5.dp)
-                                            )
-
-                                            DropdownMenuItem(
-                                                text = { Text(letter.toString()) },
-                                                onClick = {
-                                                    filterByLetter(letter)
-                                                    filterDropdownExpanded = false
-                                                }
-                                            )
-                                        }
-                                    }
-                                )
-
-                                HorizontalDivider(
-                                    color = Color.Gray,
-                                    thickness = 0.5.dp,
-                                    modifier = Modifier.padding(horizontal = 5.dp)
-                                )
-
-                                DropdownMenuItem(
-                                    text = { Text("Clear Filters") },
-                                    onClick = {
-                                        clearFilters()
-                                        filterDropdownExpanded = false
-                                    }
-                                )
-                            }
-                        }
-                    }
-                }
-
                 Card(modifier = Modifier.padding(10.dp)) {
                     LazyColumn(
                         verticalArrangement = Arrangement.spacedBy(0.dp),
@@ -376,8 +374,8 @@ fun WorldSkillsView(navController: NavController) {
                             }
                         }
 
-                        else if (hasFilteredLetter != ' ') {
-                            itemsIndexed(API.wsCache.filter { it.team.number.last() == hasFilteredLetter }) { index, wsEntry ->
+                        else if (filteredLetter != ' ') {
+                            itemsIndexed(API.wsCache.filter { it.team.number.last() == filteredLetter }) { index, wsEntry ->
 
                                 var expanded by remember { mutableStateOf(false) }
 
@@ -453,7 +451,7 @@ fun WorldSkillsView(navController: NavController) {
                                     }
                                 }
 
-                                if (index != API.wsCache.filter { it.team.number.last() == hasFilteredLetter }.size - 1) {
+                                if (index != API.wsCache.filter { it.team.number.last() == filteredLetter }.size - 1) {
                                     HorizontalDivider(
                                         thickness = 1.dp,
                                         color = MaterialTheme.colorScheme.secondary
@@ -462,9 +460,8 @@ fun WorldSkillsView(navController: NavController) {
                             }
                         }
 
-                        else if (hasFilteredRegion != 0) {
-
-                            itemsIndexed(API.wsCache.filter { it.team.eventRegionId == hasFilteredRegion }) { index, wsEntry ->
+                        else if (filteredRegion != 0) {
+                            itemsIndexed(API.wsCache.filter { it.team.eventRegionId == filteredRegion }) { index, wsEntry ->
 
                                 var expanded by remember { mutableStateOf(false) }
 
@@ -540,7 +537,7 @@ fun WorldSkillsView(navController: NavController) {
                                     }
                                 }
 
-                                if (index != API.wsCache.filter { it.team.eventRegionId == hasFilteredRegion }.size - 1) {
+                                if (index != API.wsCache.filter { it.team.eventRegionId == filteredRegion }.size - 1) {
                                     HorizontalDivider(
                                         thickness = 1.dp,
                                         color = MaterialTheme.colorScheme.secondary
