76b08adb0ee60d45899dd8244062c63e25d47367
SprintKeyz <63497433+SprintKeyz@users.noreply.github.com>
2024-02-26 13:25:03 -0500

    Add copy of the apache license for cascade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,24 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("io.github.raamcosta.compose-destinations:animations-core:1.10.0")
     ksp("io.github.raamcosta.compose-destinations:ksp:1.10.0")
+
+    // license for the cascade library:
+
+    /*
+    Copyright 2020 Saket Narayan.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    */
+
     implementation ("me.saket.cascade:cascade:2.3.0")
     implementation ("me.saket.cascade:cascade-compose:2.3.0")
 }
