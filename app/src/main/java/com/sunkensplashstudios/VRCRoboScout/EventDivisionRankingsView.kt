34ca9e0e33180181096b8e081498420787780d1f
SunkenSplash <willcas2005@icloud.com>
2024-03-16 15:04:29 -0400

    Remove unnecessary spacer in EventDivisionRankingsView UI

--- app/src/main/java/com/sunkensplashstudios/VRCRoboScout/EventDivisionRankingsView.kt
+++ app/src/main/java/com/sunkensplashstudios/VRCRoboScout/EventDivisionRankingsView.kt
@@ -2,15 +2,11 @@ package com.sunkensplashstudios.VRCRoboScout
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -19,8 +15,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -36,10 +30,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -121,7 +113,6 @@ fun EventDivisionRankingsView(event: Event, division: Division, eventDivisionRan
                 Column(
                     modifier = Modifier.verticalScroll(rememberScrollState())
                 ) {
-                    Spacer(modifier = Modifier.height(10.dp))
                     Card(
                         modifier = Modifier.padding(10.dp),
                         colors = CardColors(
