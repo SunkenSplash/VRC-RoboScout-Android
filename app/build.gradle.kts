8844748470dff487daf0617a9e80031f7b91e094
William Castro <willcas2005@icloud.com>
2024-07-02 22:07:05 -0400

    Bump version to 1.1.5 (9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sunkensplashstudios.VRCRoboScout"
         minSdk = 30
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.4"
+        versionCode = 9
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
